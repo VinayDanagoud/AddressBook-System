7d7915d370690ae8e4070dd430db2de4460df55b
VinayDanagoud <vinaydanagoud143@gmail.com>
2022-04-06 18:26:04 +0530

    Creating Contact Details

---
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace AddressBook
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstname;
+            string lastname;
+            string address;
+            string city;
+            string state;
+            string zipcode;
+            string email;
+            string phoneNumber;
+
+            Console.WriteLine("Enter the FirstName : ");
+            Console.ReadLine();
+            Console.WriteLine("Enter the lastName : ");
+            Console.ReadLine();
+            Console.WriteLine("Enter the address : ");
+            Console.ReadLine();
+            Console.WriteLine("Enter the city : ");
+            Console.ReadLine();
+            Console.WriteLine("Enter the state : ");
+            Console.ReadLine();
+            Console.WriteLine("Enter the zipcode : ");
+            Console.ReadLine();
+            Console.WriteLine("Enter the email : ");
+            Console.ReadLine();
+            Console.WriteLine("Enter the phoneNumber : ");
+            Console.ReadLine();
+
+
+        }
+
+
+    }
+}
